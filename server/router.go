2da4374968764e8223003df1f2a20dc3bd884cbf
thomas2392 <thomas.ribeiro2392@gmail.com>
2025-02-12 08:42:36 -0300

    Restart no container para tolerancia a falhas

--- server/router.go
+++ server/router.go
@@ -1,11 +1,19 @@
 package server
 
 import (
+	"fmt"
 	"loier/partilha/controllers"
 	"net/http"
+	"os"
 )
 
 func Router() {
 	http.HandleFunc("GET /health", healthCheckHandler)
 	http.HandleFunc("POST /partilha", controllers.PartilhaController)
+	http.HandleFunc("GET /crash", crashApp)
+}
+
+func crashApp(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Saindo da aplicação com erro...")
+	os.Exit(1)
 }
