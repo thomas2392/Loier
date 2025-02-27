087b16428a857c087ab697b63579f66f4ce35c97
thomas2392 <thomas.ribeiro2392@gmail.com>
2025-02-27 15:22:02 -0300

    Remove useless function

--- main.go
+++ main.go
@@ -3,22 +3,9 @@ package main
 import (
 	redis "loier/config/redis"
 	"loier/config/server"
-
-	"github.com/go-pdf/fpdf"
 )
 
 func main() {
 	redis.CreateRedisClient()
 	server.StartServer()
-}
-
-func pdf() {
-	pdf := fpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello, world!")
-	err := pdf.OutputFileAndClose("hello.pdf")
-	if err != nil {
-		panic(err)
-	}
 }
